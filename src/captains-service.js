e6a423ba3c0b128c28906708754d06a3fe35648e
nbonhamwwt <nate.bonham@wwt.com>
2026-05-21 12:34:42 -0400

    Implement getCaptains function
    This PR implements the getCaptains function to fetch captains from the API endpoint.
    
    Changes:
    - Added async getCaptains() function that calls the /captains API endpoint
    - Returns the captain data from the API response

--- src/captains-service.js
+++ src/captains-service.js
@@ -1,3 +1,6 @@
 import apiClient from './apiClient';
 
-export const getCaptains = () => null;
+export const getCaptains = async () => {
+  const response = await apiClient.get('/captains');
+  return response.data;
+};
